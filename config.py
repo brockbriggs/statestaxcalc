3cbc0cd09f0b2abaa09fe5ea8e4d3237bf8996aa
Brock Briggs <brockbrigga@gmail.com>
2026-02-18 09:56:21 -0700

    update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 import os
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-change-in-prod'
+from dotenv import load_dotenv
+
+# This looks for the .env file and loads it into os.environ
+load_dotenv() 
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
